84ab73c62171700ad52bd00ee76e4e6305d7c25e
IvesJc-Evo <ives.chiba@evosystems.com.br>
2025-06-03 18:46:25 -0300

    chore update dependencies

---
+++ src/main/java/com/adaptivedialogs/safezonejava/SafezoneJavaApplication.java
@@ -0,0 +1,13 @@
+package com.adaptivedialogs.safezonejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafezoneJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafezoneJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adaptivedialogs/safezonejava/SafezoneJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaptivedialogs.safezonejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafezoneJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
